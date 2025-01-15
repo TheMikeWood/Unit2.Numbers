ce7bef191e12f7da1ca4983d2b42ce2134ab67f2
Michael Woodard <mr.m.woodard@gmail.com>
2025-01-14 20:35:44 -0500

    finished

--- index.js
+++ index.js
@@ -11,27 +11,35 @@ class Numbers {
   }
   count() {
     //return the count of numbers in data
+    return this.data.length;
   }
   printNumbers() {
     //print the numbers in data
+    console.log(this.data);
   }
   odds() {
     //return the odd numbers in data
+    return this.data.filter((num) => num % 2 === 1);
   }
   evens() {
     //return the even numbers in data
+    return this.data.filter((num) => num % 2 === 0);
   }
   sum() {
     //return the sum of the numbers
+    return this.data.reduce((total, curr) => (total += curr), 0);
   }
   product() {
     //return the product of the numbers
+    return this.data.reduce((total, curr) => (total *= curr));
   }
   greaterThan(target) {
     //return the numbers greater than the target
+    return this.data.filter((num) => num > target);
   }
   howMany(target) {
     //return the count of a given number
+    return this.data.filter((num) => num === target).length;
   }
 }
 
